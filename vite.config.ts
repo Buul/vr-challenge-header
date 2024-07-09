a25587e4a348c99f2e857e8884c524e246677232
Paulo Silva <paulo.firmino2@gmail.com>
2024-07-09 20:28:57 -0300

    chore(Structure): add initial vite structure, with dev config

---
+++ vite.config.ts
@@ -0,0 +1,44 @@
+/// <reference types="vitest" />
+
+import path from 'path';
+
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import federation from '@originjs/vite-plugin-federation';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    react(),
+    federation({
+      name: 'header',
+      filename: 'remoteEntry.js',
+      exposes: {
+        './Header': './src/components/Header/Header',
+      },
+      shared: ['react', 'react-dom'],
+    }),
+  ],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src/'),
+    },
+  },
+  server: {
+    port: 8001,
+  },
+  root: '.',
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: ['./.test/setup.js'],
+    include: ['**/*(*.)?{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
+    exclude: ['node_modules', 'dist', '.idea', '.git', '.cache'],
+  },
+  build: {
+    modulePreload: false,
+    target: 'esnext',
+    minify: false,
+    cssCodeSplit: false,
+  },
+});
